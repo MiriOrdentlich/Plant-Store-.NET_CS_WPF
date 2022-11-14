1cb8af9ab207e4620657ea6e923b1cc54a9e6784
MiriOrdentlich <mariaordent@gmail.com>
2022-11-14 17:14:43 +0200

    Program - the very begining

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dal;
+using DO;
+/*internal*/
+class Program
+{
+    static private DalProduct TestProduct = new DalProduct();
+    static private DalOrder TestOrder = new DalOrder();
+    static private DalOrderItem TestOrderItem = new DalOrderItem();
+
+    private static void Main(string[] args)
+    {
+       
+    }
+}
