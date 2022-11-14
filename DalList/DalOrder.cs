99dc073d5b76b4310cf98d7999449d3a32ed1283
MiriOrdentlich <mariaordent@gmail.com>
2022-11-14 16:25:22 +0200

    dalOrder (unfinished) + dalProduct (unfinished)

--- DalList/DalOrder.cs
+++ DalList/DalOrder.cs
@@ -1,13 +1,97 @@
-﻿using System;
+﻿using DO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Dal;
 
-internal class DalOrder
+public class DalOrder
 {
-    string[] orderArray = new string[100];
 
-}
+    //string[] orderArray = new string[100];
+    public int Add(Order order) //create
+    {
+        if (DataSource.Config.indexOrder > 99)
+        {
+            throw new Exception("There is no more space for new orders");
+        }
+        for (int i = 0; i < DataSource.Config.indexOrder; i++)
+        {
+            
+            if (DataSource.orderArr[i].Id == order.Id)
+            {
+                throw new Exception("The identifying number already exists");
+            }
+        }
+        DataSource.orderArr[DataSource.Config.indexOrder] = order;
+        return order.Id;
+    }
+    public Order GetById(int id) //Request
+    {
+        Order p;
+        for (int i = 0; i < DataSource.Config.indexOrder; i++)
+        {
+            p = DataSource.orderArr[i];
+            if (p.Id == id)
+            {
+                return p;
+            }
+
+            
+        }
+            throw new Exception("Id Number doesn't exist");
+
+    }
+
+    public void Update(Order order)
+    {
+        for (int i = 0; i < DataSource.Config.indexOrder; i++)
+        {
+            if (DataSource.orderArr[i].Id == order.Id)
+            {
+                DataSource.orderArr[i] = order;
+            }
+            else
+                throw new Exception("The identifying number doesn't exists");
+
+        }
+
+    }
+
+    public void Delete(int id)
+    {
+        int a = -1;
+        for (int i = 0; i < 100; i++)
+        {
+
+            if (DataSource.orderArr[i].Id == id)
+            {
+                a = i;
+            }
+        }
+        if (a != -1)
+        {
+            int j = a;
+            for (; j < DataSource.Config.indexProduct - 2; j++)
+            {
+                DataSource.productArr[j] = DataSource.productArr[j + 1];
+
+            }
+            DataSource.Config.indexProduct--;
+        }
+        else
+            throw new Exception("The identifying number doesn't exists");
+    }
+
+    public IEnumerable<Order?> GetAll() //מתודת בקשה\קריאה של רשימת כל האובייקטים של הישות (ללא פרמטרים)
+    {
+        string[] onlyOrders = new string[DataSource.Config.indexOrder];
+        for (int i = 0; i < onlyOrders.Length; i++)
+        {
+            onlyOrders[i] = DataSource.orderArr[i];
+        }
+    }
+
--- DalList/DalProduct.cs
+++ DalList/DalProduct.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,90 @@ using System.Threading.Tasks;
 
 namespace Dal;
 
-internal class DalProduct
+public class DalProduct
 {
+    public int Add(Product product) //create
 
+    {
+        if (DataSource.Config.indexProduct > 49)
+        {
+            throw new Exception("There is no more space for new products");
+        for (int i = 0; i < DataSource.Config.indexProduct; i++)
+        {
 
-}
+            if (DataSource.productArr[i].Id == product.Id)
+            {
+                throw new Exception("The identifying number already exists");
+            }
+        }
+        DataSource.productArr[DataSource.Config.indexProduct] = product;
+        return product.Id;
+    }
+    public Product GetById(int id) //Request 
+    {
+        Product p;
+        for (int i = 0; i < DataSource.Config.indexProduct; i++)
+        {
+            p = DataSource.productArr[i];
+            if (p.Id == id)
+            {
+                return p;
+            }
+        }
+        throw new Exception("Id Number doesn't exist");
+    }
+
+    public void Update(Product product)
+    {
+        for (int i = 0; i < DataSource.Config.indexProduct; i++)
+        {
+            if (DataSource.productArr[i].Id == product.Id)
+            {
+                DataSource.productArr[i] = product;
+            }
+            else
+                throw new Exception("The identifying number doesn't exists");
+
+        }
+
+    }
+
+    public void Delete(int id)
+    {
+        int a = -1;
+        for (int i = 0; i < DataSource.Config.indexProduct; i++)
+        {
+
+            if (DataSource.productArr[i].Id == id)
+            {
+                a = i;
+            }
+        }
+        if (a != -1)
+            {
+                //DataSource.productArr[a] = null; //לא מדויק, צריך למחוק את מה שהוא באמת מכיל ולא רק להגיד שהוא לא מכיל כלום
+                int j = a;
+                for (; j < DataSource.Config.indexProduct - 2; j++)
+                {
+                    DataSource.productArr[j] = DataSource.productArr[j + 1];
+
+                }
+                DataSource.Config.indexProduct--;
+                //DataSource.productArr[j+1] = null; //כנל
+            }
+         else
+            throw new Exception("The identifying number doesn't exists");
+    }
+
+    public IEnumerable<Product?> [] GetAll()
+    //מתודת בקשה\קריאה של רשימת כל האובייקטים של הישות (ללא פרמטרים)
+    {
+        Product[] onlyProducts = new string[DataSource.Config.indexProduct];
+            for (int i = 0; i < onlyProducts.Length; i++)
+            {
+                onlyProducts[i] = DataSource.productArr[i];
+            }
+            return onlyProducts;
+        }
+
+    }
