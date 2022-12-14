0562e0b9c71f0ad6637f4d6d24d2f24de2c392e8
MiriOrdentlich <mariaordent@gmail.com>
2022-12-14 16:47:13 +0200

    Level 3 changes on- PL: MainWindow all the Product

--- PL/Product/ProductListWindow.xaml.cs
+++ PL/Product/ProductListWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using BlApi;
+using BO;
 using System;
+using System.Collections;
+using System.Linq;
 using System.Windows;
-
+using System.Windows.Documents;
+using System.Xml.Linq;
 
 namespace PL.Product
 {
@@ -20,16 +24,19 @@ namespace PL.Product
 
         private void CategorySelector_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-
             if (CategorySelector.SelectedItem != null)
                 ProductListView.ItemsSource = bl.Product.GetListedProducts(x => x?.Category.ToString() == CategorySelector.SelectedItem.ToString());
-          
         }
 
         private void AddProductButton_Click(object sender, RoutedEventArgs e)
         {
             ProductWindow p = new Product.ProductWindow();
-            
+            //set default values:
+            p.txtID.Text = "0";
+            p.txtPrice.Text = "0";
+            p.txtInStock.Text = "0";
+            //p.cmbCategorySelector.Text = "None";
+
             p.btnAdd.Visibility = Visibility.Visible;
             p.btnUpdate.Visibility = Visibility.Hidden; 
             p.ShowDialog();
@@ -39,7 +46,15 @@ namespace PL.Product
         private void UpdateProductButton_MouseDoubleClick(object sender, RoutedEventArgs e)
         {
             ProductWindow p = new Product.ProductWindow();
-         
+            var productList = bl.Product.GetListedProducts().ToList(); //get the current product list (which is in the same order as ProductListView)
+            var productForList = productList[ProductListView.SelectedIndex]; //the product user clicked on
+            //set default values:
+            p.txtID.Text = productForList?.Id.ToString();
+            p.txtID.IsEnabled = false;
+            p.txtPrice.Text = productForList?.Price.ToString();
+            //p.txtInStock.Text =;                            כמות מוצרים היא 0 או נאל ?????????????????????????????????
+            p.txtName.Text = productForList?.Name;
+            p.cmbCategorySelector.Text = productForList?.Category.ToString();
             p.btnAdd.Visibility = Visibility.Hidden;
             p.btnUpdate.Visibility = Visibility.Visible;
             p.ShowDialog();
