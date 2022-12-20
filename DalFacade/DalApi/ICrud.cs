841c8b4b802383ccd432057a2e27c9e4ecc474a9
Naama Schweitzer <naamasch1@gmail.com>
2022-12-20 14:35:33 +0200

    stage 4 - part 1,2

--- DalFacade/DalApi/ICrud.cs
+++ DalFacade/DalApi/ICrud.cs
@@ -9,5 +9,4 @@ public interface ICrud<T> where T : struct
     void Update(T item);
     void Delete(int id);
     IEnumerable<T?> GetAll(Func<T?, bool >? filter= null);
-
 }
--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -3,8 +3,9 @@ using DalApi;
 
 namespace Dal;
 
-sealed public class DalList : IDal
+sealed internal class DalList : IDal
 {
+    public static IDal Instance { get; } = new DalList();
     public IOrder Order => new DalOrder();
     public IProduct Product => new DalProduct();
     public IOrderItem OrderItem => new DalOrderItem();
