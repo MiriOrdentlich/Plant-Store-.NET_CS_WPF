b5bad4eaa8c776f00a3872ac323d38ed03ec7907
Naama Schweitzer <naamasch1@gmail.com>
2023-01-05 14:39:41 +0200

    order tracking window - not finished

---
+++ PL/Order/FollowOrderWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+
+namespace PL.Order
+{
+    /// <summary>
+    /// Interaction logic for FollowOrderWindow.xaml
+    /// </summary>
+    public partial class FollowOrderWindow : Window
+    {
+        private static readonly BlApi.IBl bl = BlApi.Factory.Get()!;
+
+        public BO.OrderTracking ordTrack
+        {
+            get { return (BO.OrderTracking)GetValue(ordTrackProperty); }
+            set { SetValue(ordTrackProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ordTrack.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ordTrackProperty =
+            DependencyProperty.Register("ordTrack", typeof(BO.OrderTracking), typeof(Window), new PropertyMetadata(null));
+
+        public FollowOrderWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
