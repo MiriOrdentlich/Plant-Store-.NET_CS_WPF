3bf8d16346f2125f1d7a6aac59c704b4656c809d
Naama Schweitzer <naamasch1@gmail.com>
2023-02-19 14:32:03 +0200

    stage 7

---
+++ Simulator/Simulator.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Threading;
+namespace Simulator;
+
+static internal class Simulator
+{
+    private static volatile Stopwatch stopwatch = Stopwatch.StartNew();
+    private static volatile Thread timerThread;
+    private static volatile bool isTimerRun;
+    static public void stopFunc()
+    {
+        //stopwatch.ElapsedMilliseconds;
+        if(isTimerRun)
+        {
+            stopwatch.Stop();
+            isTimerRun = false;
+        }
+    }
+
+    static public void BeginInvoke(Delegate method, params object[] args)
+    {
+        //if (!isTimerRun)
+        //{
+        //    stopwatch.Restart();
+        //    isTimerRun = true;
+
+        //    timerThread = new Thread(runTimer);
+        //    timerThread.Start();
+        //}
+        while(true)
+        {
+
+        }
+    }
+}
